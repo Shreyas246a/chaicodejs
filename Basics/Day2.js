8e4e0b37dd96d5cbb0f52eb2def18130b8a3ea8a
Shreyas246 <shreyasanchatageri2003@gmail.com>
2024-07-25 22:41:51 +0530

    Day 2 completed

---
+++ Basics/Day2.js
@@ -0,0 +1,78 @@
+//Activity 1 :Arithmetic operations
+
+//task1
+let a=4
+let b= 5
+console.log(`Addition operation ${a+b}`)
+//task2
+ a=4
+ b= 5
+console.log(`Subtraction operation ${a-b}`)
+//task3
+a=4
+let s= 5
+console.log(`Multiplication operation ${a*b}`)
+//task4
+a=4
+b = 5
+console.log(`Division operation ${a/b}`)
+
+//task 5
+a=10
+b=50
+console.log(`Remainder : ${a%b}`)
+
+//Activity 2
+//task 6
+a +=b
+console.log(`a +=b : ${a}`)
+
+//task 7
+
+b-=a
+console.log(`b-=a : ${b}`)
+
+//Activity 3
+
+//task 8
+
+let res1=a>b
+let p=20
+let q=10
+let res2=p<q
+console.log(res1)
+console.log(res2)
+
+//task 9
+
+ res1=a<b
+ p=20
+ q=20
+ res2=p<=q
+console.log(res1)
+console.log(res2)
+
+//task 10
+
+a=2
+b='2'
+res1=(a==b)
+console.log(res1)
+res2=(a===b)
+console.log(res2)
+
+//Activity 4
+//task 11
+console.log((p==q)&&(a<b))
+
+//task 12
+console.log((p==q) || (a<b))
+
+//task 13
+if(!(a<b)){
+    console.log("condition entered")
+}
+
+//task 14
+
+(a>0)?console.log(`${a} is positive`):console.log(`${a} is negative`)
