2e62527278280fa06fbe0352c54e2222ae34aedc
Shreyas246 <shreyasanchatageri2003@gmail.com>
2024-07-25 21:00:29 +0530

    Day 1 completed

--- Basics/Day1.js
+++ Basics/Day1.js
@@ -1,1 +1,36 @@
+//Activity 1
+var a=20
+let b=40
 
+console.log(a)
+console.log(b)
+
+//Activity 2
+
+const x=true
+console.log(x)
+
+//Activity 3
+
+const str="String"
+const arr=[]
+const obj={}
+const num=33
+const bool=false
+
+console.log(typeof(str))
+console.log(typeof(arr))
+console.log(typeof(obj))
+console.log(typeof(num))
+console.log(typeof(bool))
+
+//Activity 4
+let act4=40
+console.log(`Previous value ${act4}`)
+act4 = 70
+console.log(`Changed value ${act4}`)
+
+//Activity 5
+
+// const co=70
+// co=90 Assignment to constant variable Error
